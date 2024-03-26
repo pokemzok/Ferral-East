eb591f020cc29247137dd11b1443a327e9a1ed47
pokemzok <pokemzok@gmail.com>
2024-03-26 21:25:19 +0100

    refactor + pausing and unpausing the game

--- menu/main/menu.gd
+++ menu/main/menu.gd
@@ -6,15 +6,16 @@ signal start_button_pressed
 @onready var menu = %Menu
 @onready var menu_ui = $UI
 
-func _ready():
-	pause_mode = Node.PAUSE_MODE_PROCESS
-
 func _on_start_pressed():
 	emit_signal("start_button_pressed")
 
 func _on_options_pressed():
 	settings.show()
 	menu.hide()
+
+func back_to_main_menu():
+	menu.show()
+	settings.hide()
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -24,4 +25,6 @@ func hide():
 
 func toggle() -> bool:
 	menu_ui.visible = !menu_ui.visible 
+	if (menu_ui.visible):
+		back_to_main_menu()
 	return menu_ui.visible
