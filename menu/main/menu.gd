6dae7252b16327ca7b3f9f309f11ac3d1f08aa82
pokemzok <pokemzok@gmail.com>
2024-03-26 21:16:41 +0100

    refactor + showing menu during the gameplay

--- menu/main/menu.gd
+++ menu/main/menu.gd
@@ -1,17 +1,27 @@
-extends VBoxContainer
+extends Node
+
+signal start_button_pressed
 
 @onready var settings = %Settings
+@onready var menu = %Menu
+@onready var menu_ui = $UI
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file(
-		LevelManager.get_instance().get_level(LevelManager.Levels.TEST)
-	)
+	emit_signal("start_button_pressed")
 
 func _on_options_pressed():
-	print("working")
-	print(settings)
 	settings.show()
-	self.hide()
+	menu.hide()
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func hide():
+	menu_ui.hide()
+
+func toggle() -> bool:
+	menu_ui.visible = !menu_ui.visible 
+	return menu_ui.visible
