d49285a37190a2316d9504754742e20de46b1cdc
pokemzok <pokemzok@gmail.com>
2024-03-27 08:29:48 +0100

    menu improvements

--- menu/main/menu.gd
+++ menu/main/menu.gd
@@ -1,13 +1,21 @@
 extends Node
 
 signal start_button_pressed
+signal resume_button_pressed
 
 @onready var settings = %Settings
 @onready var menu = %Menu
 @onready var menu_ui = $UI
+@onready var resume_button = %Resume
+@onready var start_button = %Start
 
 func _on_start_pressed():
 	emit_signal("start_button_pressed")
+	start_button.hide()
+	resume_button.show()
+
+func _on_resume_pressed():
+	emit_signal("resume_button_pressed")
 
 func _on_options_pressed():
 	settings.show()
@@ -24,7 +32,9 @@ func hide():
 	menu_ui.hide()
 
 func toggle() -> bool:
-	menu_ui.visible = !menu_ui.visible 
-	if (menu_ui.visible):
-		back_to_main_menu()
+	if (!settings.visible && !start_button.visible):
+		menu_ui.visible = !menu_ui.visible 
+		if (menu_ui.visible):
+			back_to_main_menu()
+		return menu_ui.visible
 	return menu_ui.visible
