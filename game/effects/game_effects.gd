5d88be43419bdb380164438ab648f794f2c45540
pokemzok <pokemzok@gmail.com>
2024-07-27 20:34:22 +0200

    bullet time

--- game/effects/game_effects.gd
+++ game/effects/game_effects.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var bullet_timer = $BulletTimer
+var bullet_time_tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,13 +24,21 @@ func on_player_bought_item(item: ShopItem):
 # FIXME use tweens instead so I can gradually slow down time and resume it https://youtu.be/KB1rFrnN3-U?si=Rc2hQJ-qTabdIchY&t=83
 # FIXME LSD colors		
 func slow_time():
+	bullet_time_tween = clear_paralel_tween(bullet_time_tween)
+	bullet_time_tween.tween_property(Engine, "time_scale", 0.5, 0.5)
+	bullet_time_tween.tween_property(AudioServer, "playback_speed_scale", 0.5, 0.5)
+
 	bullet_timer.wait_time = 6
 	bullet_timer.start()
 	bullet_timer.connect("timeout", reset_time_speed)
-	Engine.time_scale = 0.5
-	AudioServer.playback_speed_scale = 0.5
-
+	
 func reset_time_speed():
 	bullet_timer.stop()
-	Engine.time_scale = 1
-	AudioServer.playback_speed_scale = 1
+	bullet_time_tween = clear_paralel_tween(bullet_time_tween)
+	bullet_time_tween.tween_property(Engine, "time_scale", 1, 0.5)
+	bullet_time_tween.tween_property(AudioServer, "playback_speed_scale", 1, 0.5)
+
+func clear_paralel_tween(tween: Tween)-> Tween:
+	if (tween != null):
+		tween.kill()
+	return create_tween().set_parallel(true)
