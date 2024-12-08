ae95bf662c523bb3fd35dc94dc2e7faa6397a4c7
pokemok <pokemzok@gmail.com>
2024-12-08 14:54:46 +0100

    demo balancing

--- character/enemy/boss/kilt_weston/kilt_weston.gd
+++ character/enemy/boss/kilt_weston/kilt_weston.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var difficulty_level = BossesMetadata.BossDifficulty.LEVEL_2
+@export var difficulty_level = BossesMetadata.BossDifficulty.LEVEL_3
 var phase = BossesMetadata.BossPhase.PHASE_1
 
 var player_detection = PlayerDetectionBehaviour.new(self)
--- character/playable/player_stats.gd
+++ character/playable/player_stats.gd
@@ -20,6 +20,7 @@ func _init(
 	self.stunned_timer = stunned_timer
 	self.reload_timer = reload_timer
 	self.health_points = health_points
+	self.accuracy = NumericAttribute.new(2.5, 5)
 	GlobalEventBus.connect(GlobalEventBus.ENEMY_DEATH, increment_kill_count)
 	GlobalEventBus.connect(GlobalEventBus.PLAYER_CONSUMED_ITEM, apply_item)
 	emit_information()
