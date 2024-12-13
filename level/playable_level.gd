91502c98d0e42c738b9aaf6c19c8481d1df811c8
pokemok <pokemzok@gmail.com>
2024-12-13 10:38:08 +0100

    dead enemies layering

---
+++ level/playable_level.gd
@@ -0,0 +1,11 @@
+class_name PlayableLevel
+extends Node
+
+var dead_enemies_container: Node
+
+func place_dead_in_same_container(details: EnemyDeathDetails):
+	var parent = details.enemy.get_parent()
+	var global_transform =  details.enemy.global_transform
+	parent.remove_child(details.enemy)
+	dead_enemies_container.add_child(details.enemy)
+	details.enemy.global_transform = global_transform
