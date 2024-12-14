22e30a120014126cea879894baba97a716538298
pokemok <pokemzok@gmail.com>
2024-12-14 18:25:36 +0100

    clouds

--- game/weather/clouds/clouds.gd
+++ game/weather/clouds/clouds.gd
@@ -1,18 +1,9 @@
-extends Node2D
-
-@onready var particles = $CPUParticles2D
+extends ParallaxBackground
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#turn_off()
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func turn_on():
-	particles.emitting = true
-
-func turn_off():
-	particles.emitting = false
