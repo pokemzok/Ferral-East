d5c6a875028a982562383111ae65aa654aed2579
pokemok <pokemzok@gmail.com>
2024-12-14 18:25:36 +0100

    clouds
    (cherry picked from commit 22e30a120014126cea879894baba97a716538298)

---
+++ game/weather/weather.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var clouds = $Clouds
+@onready var fog =  $Fog
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	turn_on_clouds()	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func turn_off_clouds():
+	clouds.hide()
+
+func turn_on_clouds():
+	clouds.show()
+	#clouds.modulate.a = 1
+
+func turn_on_fog(delta):
+	fog.show()
+	#fog.modulate.a = 1
